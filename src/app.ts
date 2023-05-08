b444f4d1c7df092a1f964175052b46795902d620
Grzegorz <g.malisz@student.fontys.nl>
2023-05-08 21:08:20 +0200

    Port changed, excluded fetching daily weather to separate function in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import ManualFetch from "./manualFetching";
 import express from "express";
 import DailyWeatherHandler from "./application/domainEventsHandlers/DailyWeatherHandler";
 import { IDailyWeatherHandler } from "./domain/interfaces/IDomainEventHandlers/IDailyWeatherHandler";
+import { DailyWeather } from "./domain/models/DailyWeather";
 
 // const manual: ManualFetch = new ManualFetch("a");
 
@@ -11,7 +12,7 @@ const dailyWeatherHandler: IDailyWeatherHandler = new DailyWeatherHandler();
 
 const app = express();
 // const PORT:number = parseInt(process.env.PORT) || 8080;
-const PORT: number = 7000;
+const PORT: number = 7001;
 
 // Middleware that parses body to JSON format
 app.use(express.json());
@@ -25,54 +26,48 @@ app.get("/dailyWeather", async (req, res) => {
   let endDate = <string>req.query.endDate;
 
   // console.log(startDate, endDate)
-  if(validateDate(startDate) && validateDate(endDate))
-  {
-    try {
-      const weather = await dailyWeatherHandler.GetDailyWeather(
-        new Date(startDate),
-        new Date(endDate)
-      );
-      return res.status(200).send(weather);
-    } catch (error) {
-      let message;
-      if (error instanceof Error) message = error.message;
-      else message = String(error);
-      // we'll proceed, but let's report it
-      console.error(message);
-      console.log(error);
-      return res.status(404).send("Wrong Dates.");
-    }
+  // Check if both dates are correct
+  if (validateDate(startDate) && validateDate(endDate)) {
+    const weather = await fetchDailyWeather(new Date(startDate), new Date(endDate));
+    return res.status(200).send(weather);
   }
-  else if(validateDate(startDate) && typeof(endDate) === 'undefined')
-  {
-    try {
-      const weather = await dailyWeatherHandler.GetDailyWeather(
-        new Date(startDate),
-        new Date()
-      );
-      return res.status(200).send(weather);
-    } catch (error) {
-      let message;
-      if (error instanceof Error) message = error.message;
-      else message = String(error);
-      // we'll proceed, but let's report it
-      console.error(message);
-      console.log(error);
-      return res.status(404).send("Wrong Dates.");
-    }
-  }
-  else{
+  // Check if first date is correct and second is not specified
+  else if (validateDate(startDate) && typeof endDate === "undefined") {
+    const weather = await fetchDailyWeather(new Date(startDate), new Date());
+    return res.status(200).send(weather);
+  } 
+  // Else return Error
+  else {
     return res.status(404).send("Wrong Dates.");
   }
 });
 
-const validateDate = (date: string) => {
-  if (typeof(date) !== 'string')
-  {
-    return false
+async function fetchDailyWeather(
+  startDate: Date,
+  endDate: Date
+): Promise<DailyWeather[]> {
+  try {
+    const weather = await dailyWeatherHandler.GetDailyWeather(
+      new Date(startDate),
+      new Date(endDate)
+    );
+    return weather;
+  } catch (error) {
+    let message;
+    if (error instanceof Error) message = error.message;
+    else message = String(error);
+    // we'll proceed, but let's report it
+    console.error(message);
+    console.log(error);
+    return Promise.reject(error);
   }
-  if (date.length != 10)
-  {
+}
+
+const validateDate = (date: string) => {
+  if (typeof date !== "string") {
+    return false;
+  }
+  if (date.length != 10) {
     return false;
   }
   return true;
